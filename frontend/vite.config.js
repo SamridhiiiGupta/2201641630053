647330dd10ebc1a64080e3a5562f54ed999cdaa0
SamridhiiiGupta <guptasamridhi1432@gmail.com>
2026-04-14 16:08:29 +0530

    updated package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    open: true,   // ← Add this line
     proxy: {
       '/api': 'http://localhost:3000',
       '/health': 'http://localhost:3000',
